901f3e413b539e0f0b7d369326f19449d0f895ee
simondelarue <sim.delarue@gmail.com>
2022-04-06 17:01:20 +0200

    get_pagerank function

--- LSBP/ranking/pagerank.py
+++ LSBP/ranking/pagerank.py
@@ -8,57 +8,66 @@ Created on April, 2021
 This code is heavily inspired from scikit-network package.
 '''
 
-import json
+
 import numpy as np
 from scipy import sparse
-from scipy.sparse.coo import coo_matrix
-from itertools import islice
-import os
 from multiprocessing import Pool
 import collections, functools, operator
-from pathlib import Path
+from itertools import repeat
+import os
+import time
 
 from LSBP.ranking.base import BaseRanking
-from LSBP.data import get_project_root, split_data, listdir_fullpath, count_degree, dict2json, json2dict
+from LSBP.data import split_data, listdir_fullpath, count_degree
 from LSBP.data import Cache
 from LSBP.utils import Bunch
+from LSBP.data import from_edge_list
 
 
 class PageRank(BaseRanking):
 
-    def __init__(self, filename: str, damping_factor: float = 0.85, solver: str = 'piteration', n_iter: int = 10, tol: float = 1e-6):
+    def __init__(self, damping_factor: float = 0.85, solver: str = 'piteration', n_iter: int = 10, tol: float = 1e-6):
         super().__init__()
         self.damping_factor = damping_factor
         self.solver = solver
         self.n_iter = n_iter 
         self.tol = tol
-        self.filename = filename
+        self.filename = ''
         self.cache = None
         self.graph = Bunch()
-        self._preprocess()
 
-        self.bipartite = None
+        '''self.bipartite = None
         self.adjacency = None
-        self.out_degrees = None
+        self.out_degrees = None'''
 
 
-    def _preprocess(self):
+    def _preprocess(self, filename: str, use_cache: bool = True):
         ''' If cache is empty, preprocesses data in parallel. If preprocessed data is already cached,
-            load information. '''
+            load information. 
+            
+        Parameters
+        ----------
+            filename: str
+                Name of data file containing list of edges.
+            use_cache: bool (default=True)
+                If True, use cached data if it exists. '''
 
-        self.cache = Cache(self.filename)
+        self.filename = filename
+        self.cache = Cache(self.filename, use_cache=use_cache)
         
-        if self.cache.is_empty:
+        if self.cache.is_empty:           
 
             # Split data into chunks
             self.graph.outdir = split_data(self.filename)
 
             # Count neighbors (parallel computing)
-            files = listdir_fullpath(self.graph.outdir)
+            self.graph.files = listdir_fullpath(self.graph.outdir)
+            
             with Pool() as p:
-                tuples = p.map(count_degree, files)
+                tuples = p.map(count_degree, self.graph.files)
 
             self.graph.nb_edges = np.sum(x[2] for x in tuples)
+
             in_degrees = dict(functools.reduce(
                                 operator.add, 
                                 map(collections.Counter, [x[0] for x in tuples])
@@ -67,7 +76,7 @@ class PageRank(BaseRanking):
                                 operator.add, 
                                 map(collections.Counter, [x[1] for x in tuples])
                                 ))
-            
+
             self.graph.nodes = np.array(list(set(in_degrees.keys()).union(set(out_degrees.keys()))))
             self.graph.nb_nodes = len(self.graph.nodes)
 
@@ -80,8 +89,8 @@ class PageRank(BaseRanking):
             for idx, k in enumerate(self.graph.nodes):
                 self.graph.label2idx[k] = idx
                 self.graph.idx2label[idx] = k
-                self.graph.in_degrees[idx] = in_degrees.get(k)
-                self.graph.out_degrees[idx] = out_degrees.get(k)
+                self.graph.in_degrees[idx] = in_degrees.get(k, 0)
+                self.graph.out_degrees[idx] = out_degrees.get(k, 0)
 
             # Cache information
             self.cache.add(self.graph)
@@ -90,11 +99,20 @@ class PageRank(BaseRanking):
             # Load information from cache
             self.graph = self.cache.load()
 
-    def fit(self, init_scores: np.ndarray = None):
+    def fit(self, filename: str, use_cache: bool = True):
+        ''' Fit algorithm to data.
+        
+        Parameters
+        ----------
+            filename: str
+                Name of data file containing list of edges.
+            use_cache: bool (default=True)
+                If True, use cached data if it exists. '''
 
-        # Format
+        # Preprocessing
+        self._preprocess(filename=filename, use_cache=use_cache)
+
         n_row, n_col = self.graph.nb_nodes, self.graph.nb_nodes
-        #self.adjacency = input_matrix
 
         # Get seeds
         seeds_row = np.ones(n_row)
@@ -107,21 +125,40 @@ class PageRank(BaseRanking):
         if seeds.sum() > 0:
             seeds /= seeds.sum()
 
-        # Out degree matrix
+        # Adjacency matrices for each batch (COO matrices)
+        iterable = list(zip([os.path.join(self.graph.outdir, f) for f in self.graph.files], repeat(self.graph.label2idx)))
+        with Pool() as p:
+            adjacencies = p.starmap(from_edge_list, iterable)
 
+        # Inverse degree matrix (COO matrix)
+        out_degrees = np.array(list(dict(sorted(self.graph.out_degrees.items(), key=lambda x: x[0], reverse=False)).values()))
+        diag: sparse.coo_matrix = sparse.diags(out_degrees, format='coo')
+        diag.data = 1 / diag.data
 
-        # Get pageRank
-        #self.scores_ = self._get_pagerank(self.adjacency, seeds, damping_factor=self.damping_factor, n_iter=self.n_iter,
-        #                                tol=self.tol, solver=self.solver, init_scores=init_scores)
-        self.scores_ = self.get_pagerank(self.graph.outdir)
+        v0 = (np.ones(n_col) - self.damping_factor) / n_col
+        init_scores = np.ones(n_row) / n_row
 
-        '''if self.bipartite:
-            self._split_vars(input_matrix.shape)'''
+        # Compute PageRank
+        for i in range(self.n_iter):
+            
+            iterable = list(zip(adjacencies, repeat(diag), repeat(self.damping_factor), repeat(init_scores), repeat(v0)))
+            with Pool() as p:
+                scores = p.starmap(self._get_pagerank, iterable)
+            
+            # Batch synchronization
+            init_scores = np.sum(scores, axis=0)
+
+        self.scores_ = init_scores
 
         return self
 
-    def _get_pagerank(self, filename: Path, seeds: np.ndarray, damping_factor: float, n_iter: int, 
-                      tol: float, init_scores: np.ndarray = None, neighbs_cnt=None) -> np.ndarray:
+    def _get_pagerank(self, adjacency: sparse.coo_matrix, diag: sparse.coo_matrix, damping_factor: float, 
+                      init_scores: np.ndarray = None, v0= np.ndarray) -> np.ndarray:
         ''' PageRank solver '''
 
+        W = (damping_factor * diag.dot(adjacency)).T.tocoo()
+
+        scores_ = W.dot(init_scores) + v0
+        scores_ /= scores_.sum()
         
+        return scores_
