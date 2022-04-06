ce930b6c4a626869a236ff40691bd8b0e3645827
simondelarue <sim.delarue@gmail.com>
2022-04-06 16:58:37 +0200

    added use_cache parameter

--- LSBP/data/cache.py
+++ LSBP/data/cache.py
@@ -16,11 +16,14 @@ from LSBP.utils import Bunch
 class Cache:
     ''' Utility for cache. '''
 
-    def __init__(self, directory: str = None):
+    def __init__(self, directory: str = None, use_cache: bool = True):
         
         data_dir = os.path.basename(directory).split('.')[0]
         self.directory = os.path.join(get_project_root(), 'preproc_data', data_dir)
         self.is_empty = not(os.path.exists(self.directory))
+
+        if not use_cache:
+            self.clear()
 
         if not self.is_empty:
             print(f'Use cached data in {self.directory}')
@@ -30,10 +33,10 @@ class Cache:
     def add(self, graph: Bunch):
         ''' Cache graph information on disk.
         
-            Parameters
-            ----------
-                graph: Bunch
-                    Graph object '''
+        Parameters
+        ----------
+            graph: Bunch
+                Graph object '''
 
         # Save information into cache
         dict2json(graph.in_degrees, os.path.join(self.directory, 'in_degrees.json'))
@@ -43,7 +46,11 @@ class Cache:
         dict2json(graph.idx2label, os.path.join(self.directory, 'idx2label.json'))
 
     def load(self) -> Bunch:
-        ''' Load cached information into a Bunch object. '''
+        ''' Load cached information into a Bunch object. 
+        
+        Output
+        ------
+            graph as a Bunch class. '''
 
         graph = Bunch()
 
@@ -51,10 +58,11 @@ class Cache:
         graph.outdir = self.directory
         graph.label2idx = json2dict(os.path.join(self.directory, 'label2idx.json'))
         graph.idx2label = json2dict(os.path.join(self.directory, 'idx2label.json'))
-        graph.in_degrees = json2dict(os.path.join(self.directory, 'in_degrees.json'))
-        graph.out_degrees = json2dict(os.path.join(self.directory, 'out_degrees.json'))
+        graph.in_degrees = json2dict(os.path.join(self.directory, 'in_degrees.json'), keys_int=True)
+        graph.out_degrees = json2dict(os.path.join(self.directory, 'out_degrees.json'), keys_int=True)
         graph.nodes = np.load(os.path.join(self.directory, 'nodes.npy'))
         graph.nb_nodes = len(graph.nodes)
+        graph.files = [f for f in os.listdir(self.directory) if os.path.basename(self.directory) in f]
 
         return graph
 
