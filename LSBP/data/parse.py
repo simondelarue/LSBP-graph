4741b2f4c5f6bba35a51affa02b7a9dc76561ab1
simondelarue <sim.delarue@gmail.com>
2022-04-07 15:16:56 +0200

    updated from_edge_list

--- LSBP/data/parse.py
+++ LSBP/data/parse.py
@@ -9,8 +9,9 @@ Created on April 2021
 import numpy as np
 from scipy import sparse
 
+from LSBP.data.load import sniff_delimiter
 
-def from_edge_list(filename: str, reindex: dict) -> sparse.coo_matrix:
+def from_edge_list_old(filename: str, reindex: dict) -> sparse.coo_matrix:
     ''' Reads edge list and parse it to scipy COO format.
     
     Parameters
@@ -33,6 +34,33 @@ def from_edge_list(filename: str, reindex: dict) -> sparse.coo_matrix:
             if vals[0] != '' and vals[0] != 'Source':
                 rows.append(reindex.get(vals[0]))
                 cols.append(reindex.get(vals[1]))
+    data = np.ones(len(rows))
+
+    return sparse.coo_matrix((data, (rows, cols)), shape=(n, n))
+
+def from_edge_list(filename: str, n: int) -> sparse.coo_matrix:
+    ''' Reads edge list and parse it to scipy COO format.
+    
+    Parameters
+    ----------
+        filename: str
+            Name of data file containing list of edges separated by commas.
+        n: int
+            Number of nodes.
+
+    Output
+    ------
+        Adjacency matrix in sparse.COO format. '''
+    
+    rows, cols = [], []
+    
+    with open(filename) as f:
+        delimiter = sniff_delimiter(filename)
+        for line in f:
+            vals = line.strip('\n').split(delimiter)
+            if vals[0] != '' and vals[0] != 'Source':
+                rows.append(vals[0])
+                cols.append(vals[1])
     data = np.ones(len(rows))
 
     return sparse.coo_matrix((data, (rows, cols)), shape=(n, n))
