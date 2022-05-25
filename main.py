d74a98ee8c3c62b8ed1843706b9471a6c4ff600e
simondelarue <sim.delarue@gmail.com>
2022-05-25 15:28:08 +0200

    main modified

--- main.py
+++ main.py
@@ -26,39 +26,50 @@ if __name__=='__main__':
     filename = f'{DATA_DIR}/soc-LiveJournal1.txt'
 
     method = 'topk'
-    # What it does under the hood:
+    # If 'split': What it does under the hood ?
     # - preprocess, i.e divide data into chunks of edges
     # - compute PageRank on each chunk
 
     s = time.time()
-    pr = PageRank(n_iter=5)
+    pr = PageRank()
     scores = pr.fit_transform(filename, use_cache=True, method=method)
     topk_scores_idx = np.argsort(-scores)[:10]
-    print(max(scores), min(scores))
+    #print(max(scores), min(scores))
     #pr.filter_transform(filename, method='indegree')
     
     print('-------------------------------')
-    print(f'Completed in {(time.time()-s):.4f}s')
+    print(f'Total computation time: {(time.time()-s):.4f}s')
     print('-------------------------------')
     
     print(f'Method: {method}')
-    print(f'Number of nodes: {pr.graph.nb_nodes}')
+    print(f'Number of nodes: {pr.graph.nb_nodes} - Number of edges: {pr.graph.nb_edges}')
     #print(f'Number of edges: {pr.graph.nb_edges}')
 
     nodes_stream = []
     for idx in topk_scores_idx:
-        print(f'Node: {pr.graph.idx2label.get(idx)} - {scores[idx]}')
-        nodes_stream.append(pr.graph.idx2label.get(idx))
+        print(f'Node: {pr.graph.idx2label.get(pr.graph.idx2label_densest.get(idx))} - {scores[idx]}')
+        nodes_stream.append(pr.graph.idx2label.get(pr.graph.idx2label_densest.get(idx)))
 
-    print('-------------------------------')
+    #raise Exception('end here')
+
+    print('\n-------------------------------')
     print('Sknetwork')
+    print('-------------------------------')
+    print('Preprocessing...')
     start = time.time()
     filename = '/Users/simondelarue/Documents/PhD/Research/LSBP-graph/data/soc-LiveJournal1.txt'
     df = pd.read_csv(filename, delimiter='\t', names=['src', 'dst'], skiprows=1)
     edge_list = list(df.itertuples(index=False))
     graph = convert_edge_list(edge_list, directed=True)
+    print(f'Completed in {(time.time()-start):.4f}s')
+
+    print('PageRank...')
+    start = time.time()
     pr = skPageRank()
     scores_pr = pr.fit_transform(graph.adjacency)
+    print(f'Completed in {(time.time()-start):.4f}s')
+    print(f'Number of nodes: {graph.adjacency.shape[0]} - Number of edges: {graph.adjacency.nnz}')
+    
     for s, n in zip(scores_pr[np.argsort(-scores_pr)[:10]], np.argsort(-scores_pr)[:10]):
         print(f'Node: {n} - score: {s}')
 
@@ -66,20 +77,33 @@ if __name__=='__main__':
     stream_top10 = set([int(i) for i in nodes_stream])
     print(sk_top10)
     print(stream_top10)
+    print('-------------------------------')
+    print('Metrics')
+    print('-------------------------------')
     print('Common ratio: ', len(sk_top10.intersection(stream_top10))/10)
 
-    # Compute score on top10
+    # Compute accuracy on top-k nodes
     ks = [10, 100, 1000]
+    pred_rnks = np.argsort(-scores)
+    for k in ks:
+        sk_topk = set(np.argsort(-scores_pr)[:k])
+        topk_scores_idx = np.argsort(-scores)[:k]
+        nodes_stream = []
+        for idx in topk_scores_idx:
+            nodes_stream.append(pr.graph.idx2label.get(pr.graph.idx2label_densest.get(idx)))
+        stream_topk = set([int(i) for i in nodes_stream])
+        print(f'Common ratio for {k} nodes: ', len(sk_topk.intersection(stream_topk))/k)
+
+    # Compute NDCG on top-k nodes
+    """ks = [10, 100, 1000]
     pred_rnks = np.argsort(-scores)
     for k in ks:
         true_top_idx = np.argsort(-scores_pr)[:k]
         true_rnks = np.asarray([np.arange(0, k)])
         true_top_preds_rnks = np.asarray([[np.where(pred_rnks == i) for i in true_top_idx]])
         NDCG = ndcg_score(true_rnks, true_top_preds_rnks)
-        print(f'NDCG score for k={k}: {NDCG:.4f}')
+        print(f'NDCG score for k={k}: {NDCG:.4f}')"""
 
-
-    print(f'Completed in {(time.time()-start):.4f}s')
     print('-------------------------------')
 
     """for idx, s in enumerate((scores)):
